2851a88651606407527520b8c464febb83a623e6
yongsong cao <62881133+Rowling7@users.noreply.github.com>
2021-03-14 09:19:49 +0800

    Delete app directory

--- app/src/main/java/com/example/administrator/shopping/Impl/OnEditBtnClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.administrator.shopping.Impl;
-
-import android.view.View;
-
-
-public interface OnEditBtnClickListener {
-    void onEditBtnClick(View view, int position);   // 修改
-}
--- app/src/main/java/com/example/administrator/shopping/Impl/OnInsBtnClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.administrator.shopping.Impl;
-
-import android.view.View;
-
-public interface OnInsBtnClickListener {
-
-    void OnInsBtnClick(View view, int position);   // 添加购物车
-}
